50d324574897fb50213b67bd6f060d5c566ecc1b
Jansen Rodriguez <JansenR125@gmail.com>
2021-01-21 18:22:29 -0500

    Added class to test deletion configs

---
+++ com/luv2code/hibernate/demo/DeletePacmanCourse.java
@@ -0,0 +1,52 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Review;
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class DeletePacmanCourse {
+
+	public static void main(String[] args) {
+		
+		SessionFactory factory = new Configuration()
+								 .configure("hibernate.cfg.xml")
+								 .addAnnotatedClass(Instructor.class)
+								 .addAnnotatedClass(InstructorDetail.class)
+								 .addAnnotatedClass(Course.class)
+								 .addAnnotatedClass(Review.class)
+								 .addAnnotatedClass(Student.class)
+								 .buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// Start transaction
+			session.beginTransaction();
+			
+			// Get the pacman course
+			int courseID = 10;
+			Course tempCourse = session.get(Course.class, courseID);
+			
+			// Delete the course (NOTE: Students taking course should not be deleted due to cascade configs)
+			System.out.println("Deleting course: " + tempCourse);
+			session.delete(tempCourse);
+			
+			// Commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
