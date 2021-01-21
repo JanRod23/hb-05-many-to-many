ad80bdb9060ac0add63fad0d11072c50adce87da
Jansen Rodriguez <JansenR125@gmail.com>
2021-01-21 17:39:33 -0500

    Setting up project

---
+++ com/luv2code/hibernate/demo/GetCourseAndReviewsDemo.java
@@ -0,0 +1,53 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Review;
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class GetCourseAndReviewsDemo {
+
+	public static void main(String[] args) {
+		
+		SessionFactory factory = new Configuration()
+								 .configure("hibernate.cfg.xml")
+								 .addAnnotatedClass(Instructor.class)
+								 .addAnnotatedClass(InstructorDetail.class)
+								 .addAnnotatedClass(Course.class)
+								 .addAnnotatedClass(Review.class)
+								 .buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// Start transaction
+			session.beginTransaction();
+			
+			// Get the course
+			int courseID = 10;
+			Course tempCourse = session.get(Course.class, courseID);
+			
+			// Print course
+			System.out.println(tempCourse);
+			
+			// Print course reviews
+			System.out.println(tempCourse.getReviews());
+			
+			// Commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
